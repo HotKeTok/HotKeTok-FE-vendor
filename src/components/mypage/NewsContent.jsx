1359b0b9fce084d94b78bc65536f2ba05784e377
yminjuu <ymj0580@naver.com>
2025-10-09 23:48:20 +0900

    feat: 기본 요소 파일 추가

---
+++ src/components/mypage/NewsContent.jsx
@@ -0,0 +1,8 @@
+export default function NewsContent({ newsData }) {
+  return (
+    <div>
+      <h2>소식</h2>
+      <p>소식 콘텐츠 내용</p>
+    </div>
+  );
+}
---
+++ src/components/mypage/ReviewContent.jsx
@@ -0,0 +1,8 @@
+export default function ReviewContent({ reviewData }) {
+  return (
+    <div>
+      <h2>리뷰</h2>
+      <p>리뷰 콘텐츠 내용</p>
+    </div>
+  );
+}
