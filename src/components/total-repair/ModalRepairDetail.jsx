cce14db7b801c5a69435c9f5c0d5c027fd211fb6
sunwoo-h <flexible8011@gmail.com>
2025-10-27 00:06:45 +0900

    refactor: total-repair 관련 컴포넌트 파일 이동

--- src/components/total-repair/ModalRepairDetail.jsx
+++ src/components/total-repair/ModalRepairDetail.jsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 import { format } from 'date-fns';
 import { ko } from 'date-fns/locale';
 import { color, typo } from '../../styles/tokens';
-import RepairStatusChip from './RepairStatusChip';
+import RepairStatusChip from '../common/RepairStatusChip';
 import { Column, Row } from '../../styles/flex';
-import Button from './Button';
-import Modal from './Modal';
+import Button from '../common/Button';
+import Modal from '../common/Modal';
 import { formatPhone } from '../../utils/format';
 
 /**
